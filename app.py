edd2dd8ff1e1b14a4c22bda5ec92d4d20c02fc23
poornimakumaran <139451899+poornimakumaran@users.noreply.github.com>
2024-05-25 20:38:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,96 +1,61 @@
 from flask import Flask, request, render_template
 import re
 import math
+import os
 
-app = Flask("__name__")
-
-q = ""
+app = Flask(__name__)
 
 @app.route("/")
 def loadPage():
-	return render_template('index.html', query="")
+    return render_template('index.html', query="")
 
 @app.route("/", methods=['POST'])
 def cosineSimilarity():
-	try:
-		universalSetOfUniqueWords = []
-		matchPercentage = 0
-    	
-		####################################################################################################
-		
-		inputQuery = request.form['query']
-		lowercaseQuery = inputQuery.lower()
-    	
-		queryWordList = re.sub("[^\w]", " ",lowercaseQuery).split()			#Replace punctuation by space and split
-		# queryWordList = map(str, queryWordList)					#This was causing divide by zero error
-    	
-		for word in queryWordList:
-			if word not in universalSetOfUniqueWords:
-				universalSetOfUniqueWords.append(word)
-    	
-		####################################################################################################
-    	
-		fd = open("database1.txt", "r")
-		database1 = fd.read().lower()
-    	
-		databaseWordList = re.sub("[^\w]", " ",database1).split()	#Replace punctuation by space and split
-		# databaseWordList = map(str, databaseWordList)			#And this also leads to divide by zero error
-    	
-		for word in databaseWordList:
-			if word not in universalSetOfUniqueWords:
-				universalSetOfUniqueWords.append(word)
-    	
-		####################################################################################################
-    	
-		queryTF = []
-		databaseTF = []
-    	
-		for word in universalSetOfUniqueWords:
-			queryTfCounter = 0
-			databaseTfCounter = 0
-    	
-			for word2 in queryWordList:
-				if word == word2:
-					queryTfCounter += 1
-			queryTF.append(queryTfCounter)
-    	
-			for word2 in databaseWordList:
-				if word == word2:
-					databaseTfCounter += 1
-			databaseTF.append(databaseTfCounter)
-    	
-		dotProduct = 0
-		for i in range (len(queryTF)):
-			dotProduct += queryTF[i]*databaseTF[i]
-    	
-		queryVectorMagnitude = 0
-		for i in range (len(queryTF)):
-			queryVectorMagnitude += queryTF[i]**2
-		queryVectorMagnitude = math.sqrt(queryVectorMagnitude)
-    	
-		databaseVectorMagnitude = 0
-		for i in range (len(databaseTF)):
-			databaseVectorMagnitude += databaseTF[i]**2
-		databaseVectorMagnitude = math.sqrt(databaseVectorMagnitude)
-    	
-		matchPercentage = (float)(dotProduct / (queryVectorMagnitude * databaseVectorMagnitude))*100
-    	
-		'''
-		print queryWordList
-		print
-		print databaseWordList
-    	
-    	
-		print queryTF
-		print
-		print databaseTF
-		'''
-    	
-		output = "Input query text matches %0.02f%% with database."%matchPercentage
-    	
-		return render_template('index.html', query=inputQuery, output=output)
-	except Exception as e:
-		output = "Please Enter Valid Data"
-		return render_template('index.html', query=inputQuery, output=output)
+    try:
+        universalSetOfUniqueWords = []
+        matchPercentage = 0
 
-app.run()
+        # Retrieve input query from form
+        inputQuery = request.form['query']
+        lowercaseQuery = inputQuery.lower()
+
+        # Process query text
+        queryWordList = re.sub("[^\w]", " ", lowercaseQuery).split()
+        for word in queryWordList:
+            if word not in universalSetOfUniqueWords:
+                universalSetOfUniqueWords.append(word)
+
+        # Read and process database text
+        with open("database1.txt", "r") as fd:
+            database1 = fd.read().lower()
+        databaseWordList = re.sub("[^\w]", " ", database1).split()
+        for word in databaseWordList:
+            if word not in universalSetOfUniqueWords:
+                universalSetOfUniqueWords.append(word)
+
+        # Calculate term frequencies
+        queryTF = []
+        databaseTF = []
+
+        for word in universalSetOfUniqueWords:
+            queryTF.append(queryWordList.count(word))
+            databaseTF.append(databaseWordList.count(word))
+
+        # Compute dot product and magnitudes
+        dotProduct = sum(q * d for q, d in zip(queryTF, databaseTF))
+        queryVectorMagnitude = math.sqrt(sum(q ** 2 for q in queryTF))
+        databaseVectorMagnitude = math.sqrt(sum(d ** 2 for d in databaseTF))
+
+        # Calculate match percentage
+        matchPercentage = (dotProduct / (queryVectorMagnitude * databaseVectorMagnitude)) * 100 if queryVectorMagnitude and databaseVectorMagnitude else 0.0
+
+        output = "Input query text matches %0.02f%% with database." % matchPercentage
+
+        return render_template('index.html', query=inputQuery, output=output)
+    except Exception as e:
+        output = "Please Enter Valid Data"
+        return render_template('index.html', query=inputQuery, output=output)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
